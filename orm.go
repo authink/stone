4ded016a17838dc95adfe1e57b93e2b0f39d0170
huoyijie <yijie.huo@foxmail.com>
2024-02-29 18:32:35 +0800

    refactor orm interfice

--- orm.go
+++ orm.go
@@ -6,13 +6,25 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+type ModelOf interface {
+	Of() *Model
+}
+
 type Model struct {
 	Id        uint32
 	CreatedAt *time.Time `db:"created_at"`
 	UpdatedAt *time.Time `db:"updated_at"`
 }
 
+func (m *Model) Of() *Model {
+	return m
+}
+
+var _ ModelOf = (*Model)(nil)
+
 type ORM[T any] interface {
+	Insert(*T) error
+	InsertWithTx(*T, *sqlx.Tx) error
 	Save(*T) error
 	SaveWithTx(*T, *sqlx.Tx) error
 	Get(int) (*T, error)
@@ -35,6 +47,7 @@ func Transaction(app *AppContext, txFunc TxFunc) (err error) {
 
 type SQL interface {
 	Insert() string
+	Save() string
 	Delete() string
 	Update() string
 	Get() string
